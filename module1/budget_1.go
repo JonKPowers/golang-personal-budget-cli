cb8afc12602db4271add0f8521dea8d90b357097
Jon Powers <Jon.K.Powers+git@gmail.com>
2021-10-17 16:30:35 -0400

    Module 1 push

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,14 @@
 package module1
 
 // Budget stores budget information
+struct Budget {
+	Max float32
+	Items []Item
+}
+
 
 // Item stores item information
+struct Item {
+	Description string
+	Price float32
+}
